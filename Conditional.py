4fc0607e267b02aaa71ee24bd5fac95d637279e2
shaanpatel-ai <shaanpatelnz@gmail.com>
2026-07-14 18:54:19 +1200

    odd or even

---
+++ Conditional.py
@@ -0,0 +1,8 @@
+
+
+# Write a Python program to first take a number as an input from the user, then check whether that number is even or odd and print the result.
+num=int(input("Enter a number"))
+if num %2==0:
+    print(num,"is a even number")
+else:
+    print(num,"is an odd number")
