b0c8736c09f9ef892496a5f6d44e01c89587f86e
shaanpatel-ai <shaanpatelnz@gmail.com>
2026-07-21 19:12:13 +1200

    functions

---
+++ loop.py
@@ -0,0 +1,15 @@
+# n=int(input(" enter a number to see the table of that number "))
+# for i in range(1,11):
+#     print(n, "x",i,"=",i*n)
+
+# print("here's the numbers that are even in 1 to 10")
+#     li=[1,2,3,4,5,6,7,8,9,10]
+#     for i in li:
+#         if i%2==0:
+#             print(i)
+i=1
+sum=0
+while i<11:
+    sum=sum+i
+    i=i+1
+print("Sum of first 10 naturl numers is",sum)
